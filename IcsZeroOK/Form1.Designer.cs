e8d6d1844af59f2d29f3c7f3018ae0122a982e70
antoniu.pitic <antoniu.pitic@gmail.com>
2021-07-07 11:43:30 +0300

    - generare de table de joc - setarea proprietatilor - setare de functii pe unele evenimente - lispseste jocul in sine de pe Click

--- IcsZeroOK/Form1.Designer.cs
+++ IcsZeroOK/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace IcsZeroOK
         private void InitializeComponent()
         {
             this.newGameButton = new System.Windows.Forms.Button();
+            this.testButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // newGameButton
@@ -42,11 +43,22 @@ namespace IcsZeroOK
             this.newGameButton.UseVisualStyleBackColor = true;
             this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
             // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(328, 327);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 1;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(462, 382);
+            this.Controls.Add(this.testButton);
             this.Controls.Add(this.newGameButton);
             this.Name = "mainForm";
             this.Text = "TicTacToe";
@@ -57,6 +69,7 @@ namespace IcsZeroOK
         #endregion
 
         private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button testButton;
     }
 }
 
--- IcsZeroOK/Form1.cs
+++ IcsZeroOK/Form1.cs
@@ -13,6 +13,7 @@ namespace IcsZeroOK
     public partial class mainForm : System.Windows.Forms.Form
     {
         int currentUser;
+        Label[,] board = new Label[3, 3];
         public mainForm()
         {
             InitializeComponent();
@@ -31,7 +32,54 @@ namespace IcsZeroOK
 
         private void CreateBoard()
         {
-            Console.WriteLine("aici cica se creaza matricea de 3x3");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = new Label();
+
+                    this.board[i, j].BackColor = System.Drawing.Color.Lime;
+                    this.board[i, j].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    this.board[i, j].Left = 100 + j * 54;
+                    this.board[i, j].Top = 50 + i * 54;
+                    this.board[i, j].Size = new System.Drawing.Size(50, 50);
+                    this.board[i, j].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+                    this.board[i, j].Click += Play;
+                    this.board[i, j].MouseEnter += OnMouseEnter;
+                    this.board[i, j].MouseLeave += OnMouseLeave;
+
+
+                    Controls.Add(board[i, j]);
+                }
+            }
+
+        }
+
+        private void Play(object sender, EventArgs e)
+        {
+            Console.WriteLine("Am facut click");
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.Lime;            
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.Yellow;
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            Label testLabel = new Label();
+
+            testLabel.Text = "Franzela";
+            testLabel.Left = 100;
+            testLabel.Top = 20;
+
+            this.Controls.Add(testLabel);
         }
     }
 }
